8e7593058c80d1dd99305fa5017ad3d5c85c0f01
AKHIL RAJ SRIVASTAVA <84132589+AKHILRAJSRIVASTAVA@users.noreply.github.com>
2022-10-18 01:06:41 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,12 @@
+from flask import Flask , jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/',methods=['GET'])
+def predict():
+    response = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+    return jsonify(response)
+
+if __name__=='__main__':
+    app.run(debug=True)
