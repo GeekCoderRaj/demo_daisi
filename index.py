9eb0cd51331ce292730a36b5c860fcb252711462
AKHIL RAJ SRIVASTAVA <84132589+AKHILRAJSRIVASTAVA@users.noreply.github.com>
2022-10-18 18:31:22 +0530

    ..

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/',methods=['GET'])
 def predict():
     response = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
-    return jsonify(response)
+    return response
 
 if __name__=='__main__':
     app.run(debug=True)
---
+++ index2.py
@@ -0,0 +1,3 @@
+import pydaisi as pyd
+demo3 = pyd.Daisi("geekcoderraj/demo3")
+demo3.predict().value
